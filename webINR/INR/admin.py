76494add878c94a0c4c69b3009ee1199f9822755
fdiazmlg <fernandodiaz@uma.es>
2017-04-19 23:54:47 +0200

    Added multidb handler for Auth [UNDER TESTING]

--- webINR/INR/admin.py
+++ webINR/INR/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Comentario, PacienteClinica, Diagnostico, Medicacion, Visita
 
+
+"""class AuthDBModelAdmin(admin.ModelAdmin):
+	# Customized DBModel for multi-db compatibility
+
+    using = 'Auth'
+
+    
+    def save_model(self, request, obj, form, change):
+    	# save_model to database Auth
+        obj.save(using=self.using)
+
+    def delete_model(self, request, obj):
+    	# delete_model from database Auth
+        obj.delete(using=self.using)
+
+    def get_queryset(self, request):
+    	# query to Auth
+        return super(AuthDBModelAdmin, self).get_queryset(request).using(self.using)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    	# Populate foreignkeys using a query 
+        return super(AuthDBModelAdmin, self).formfield_for_foreignkey(db_field, request, using=self.using, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+    	# Populate M2M widgets using a query
+        return super(AuthDBModelAdmin, self).formfield_for_manytomany(db_field, request, using=self.using, **kwargs)
+"""
+
 admin.site.register(Comentario)
 admin.site.register(PacienteClinica)
 admin.site.register(Diagnostico)
