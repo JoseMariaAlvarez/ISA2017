9d920592c34526fcf76d0e18ca358cbdb135c46a
fdiazmlg <fernandodiaz@uma.es>
2017-04-24 22:58:59 +0200

    Added django auth for regular users

--- webINR/webINR/urls.py
+++ webINR/webINR/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^', include("INR.urls")),
 ]
