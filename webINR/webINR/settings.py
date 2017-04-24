19bb0689c92393d8a168eeaba5b11035366dade6
fdiazmlg <fernandodiaz@uma.es>
2017-04-24 21:52:11 +0200

    Added custom backend to settings

--- webINR/webINR/settings.py
+++ webINR/webINR/settings.py
@@ -100,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'webINR.backend.LoginBackend',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
