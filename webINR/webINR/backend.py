fb9e16100362b10d7336b35179507419e1e532e1
Antonio Benitez <antbenhid@uma.es>
2017-04-26 19:03:19 +0200

    Añadido campo puerto y editada query

--- webINR/webINR/backend.py
+++ webINR/webINR/backend.py
@@ -7,18 +7,22 @@ class LoginBackend(object):
     def authenticate(self, request, username=None, password=None):
         try:
             connection = MySQLConn(
-                database="usuariossanitarios", username='root', password='root')
+                host="localhost", database="usuariossanitarios", username='root', password='mysql', port=3306)
             cursor = connection.cursor
 
-            query = ('SELECT usuario, password, nombre, apellido1 FROM usuarios_sanitarios WHERE usuario="?" AND password="?"',(
-                request.POST['username'], request.POST['password']))
+            query = 'SELECT usuario, password, nombre, apellido1 FROM usuarios_sanitarios WHERE usuario="%s" AND password="%s"' % (request.POST['username'], request.POST['password'])
             cursor.execute(query)
+
+            user = None
+
             for usuario, password, nombre, apellido1 in cursor:
-                u, c = User.objects.get_or_create(
+                user, created = User.objects.get_or_create(
                     username=usuario, password=password, first_name=nombre, last_name=apellido1)
+            return user
+
         except User.DoesNotExist:
             return None
-        return u
+
 
     def get_user(self, user_id):
         try:
