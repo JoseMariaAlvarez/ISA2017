1e4fe99c586f2284fa27b02d6f63a64092d602d8
fdiazmlg <fernandodiaz@uma.es>
2017-04-27 20:28:02 +0200

    Added alta url

--- webINR/INR/urls.py
+++ webINR/INR/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^ficha/$', views.ficha, name='ficha'),
+    url(r'^alta/$', views.dar_alta, name='alta'),
 ]
