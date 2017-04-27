689f53b900b4fe85f5a40d029d445371736d0555
Antonio Benitez <antbenhid@uma.es>
2017-04-27 14:04:16 +0200

    Nueva página

--- webINR/INR/views.py
+++ webINR/INR/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def index(request):
     return render(request, 'base.html')
+
+@login_required
+def ficha(request):
+    return render(request, 'pages/ficha_de_paciente.html')
