ff84d600ec630d461d06896c81409bc60eaeedd5
fdiazmlg <fernandodiaz@uma.es>
2017-05-01 19:08:08 +0200

    Added views for Visita

--- webINR/INR/urls.py
+++ webINR/INR/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^ficha/$', views.ver_ficha, name='ficha'),
     url(r'^alta/$', views.dar_alta, name='alta'),
     url(r'^buscar/$', views.buscar, name='busqueda'),
-    url(r'^visitas/(?P<nss>\w{0,50})/$', views.get_citas, name='visita'),
+    url(r'^visitas/(?P<nss>\w{0,50})/$', views.get_visitas, name='visita'),
+    url(r'^modvisitas/(?P<id>\w{0,50})/$', views.cambiar_visita, name='modvisitas'),
 ]
