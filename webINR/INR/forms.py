a5487ce3e9b671bed55188a01276da820b915935
fdiazmlg <fernandodiaz@uma.es>
2017-05-02 12:58:39 +0200

    Updated docstrings.

--- webINR/INR/forms.py
+++ webINR/INR/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Visita
 
+# Alta Form.
 class AltaForm(forms.Form):
     SEARCH_CHOICES = [('dni', 'DNI'),
                       ('nss', 'NSS')]
@@ -9,15 +10,7 @@ class AltaForm(forms.Form):
     nss = forms.CharField(label='NSS', max_length=20, widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
     dni = forms.CharField(label='DNI', max_length=10, widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
 
-"""class Visita(forms.Form):
-	valorINR = forms.DecimalField(label='Valor INR', max_digits=20, decimal_places=10)
-	fecha = forms.DateField(label='Fecha')
-	dosis = forms.DecimalField(label='Dosis', max_digits=20, decimal_places=10)
-	duracion = forms.CharField(label='Duracion', max_length=45)
-	peso = forms.DecimalField(label='Peso', max_digits=20, decimal_places=10)
-	rango = forms.CharField(label='Rango', max_length=45)""" 
-
-
+# Inherits from Visita object to instance the database, avoiding duplicates.
 class VisitaForm(forms.ModelForm):
 	class Meta:
 		model = Visita
