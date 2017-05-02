57c0f44ddef58016586b6a0c9c7a689f2abbf840
Antonio B <makishinake@hotmail.com>
2017-05-02 18:30:17 +0200

    Ver ficha de un paciente

--- webINR/INR/urls.py
+++ webINR/INR/urls.py
@@ -18,7 +18,5 @@ urlpatterns = [
     url(r'^ficha/$', views.ver_ficha, name='ficha'),
     url(r'^alta/$', views.dar_alta, name='alta'),
     url(r'^buscar/$', views.buscar, name='busqueda'),
-    url(r'^visitas/(?P<nss>\w{0,50})/$', views.get_visitas, name='visita'),
-    url(r'^modvisitas/(?P<id>\w{0,50})/$', views.cambiar_visita, name='modvisitas'),
-    url(r'^datos_paciente/(?P<nss>\w{0,50})/$', views.get_datos_demograficos, name='datos_paciente'),
+    url(r'^modvisitas/(?P<id>\w{0,50})/$', views.cambiar_visita, name='modvisitas')
 ]
--- webINR/INR/views.py
+++ webINR/INR/views.py
@@ -15,6 +15,7 @@ import string
 def index(request):
     return render(request, 'base.html')
 
+
 @login_required
 def ver_ficha(request):
     ## Nos muestra la página ficha_de_paciente.html
@@ -33,15 +34,15 @@ def dar_alta(request):
         if form.is_valid():  ## ...si el formulario es válido...
             ## ...establecemos conexión con la base de datos.
             connection = MySQLDriver.MySQLConn(
-                host="localhost", database="usuariossanitarios", username="root", password="angel", port=3306)
+                host="localhost", database="usuariossanitarios", username="root", password="root", port=3306)
             cursor = connection.cursor
 
             ## La query cambiará según estemos buscando por dni o por nss
             if form.cleaned_data['query_choice'] == 'dni':
-                query = 'SELECT nss,dni,nombre,apellido1 FROM pacientes WHERE dni=\"%s\"' % form.cleaned_data[
+                query = 'SELECT nss, dni, nombre, apellido1, apellido2, direccion, cp, telefono, ciudad, provincia, pais, fecha_nacimiento, sexo FROM pacientes WHERE  dni=\"%s\"' % form.cleaned_data[
                     'dni']
             else:
-                query = 'SELECT nss,dni,nombre,apellido1 FROM pacientes WHERE nss=\"%s\"' % form.cleaned_data[
+                query = 'SELECT nss, dni, nombre, apellido1, apellido2, direccion, cp, telefono, ciudad, provincia, pais, fecha_nacimiento, sexo FROM pacientes WHERE nss=\"%s\"' % form.cleaned_data[
                     'nss']
 
             ## Ejecutamos la query
@@ -56,10 +57,11 @@ def dar_alta(request):
 
             ## Creamos el usuario usando los parámetros obtenidos de la query o lo recuperamos en caso de ya existir (get_or_create())
             PacienteClinica.objects.get_or_create(
-                nss=row[0], dni=row[1], nombre=row[2], apellido_1=row[3], password=password)
+                nss=row[0], dni=row[1], nombre=row[2], apellido_1=row[3], apellido_2=row[4], direccion=row[5], cp=row[6], telefono=row[7],
+                ciudad=row[8], provincia=row[8], pais=row[10], fecha_nacimiento=row[11], sexo=row[12], password=password)
 
-            connection.close()
-            return HttpResponse('Paciente dado de alta en la base de datos.')
+            #connection.close()
+            return render(request, 'pages/alta_de_paciente.html', {'success': 'Paciente dado de alta en la base de datos'})
     else:
         form = AltaForm()
 
@@ -82,20 +84,17 @@ def buscar(request):
             else:
                 res = get_object_or_404(PacienteClinica, nss=form.cleaned_data['nss'])
 
-            return render(request, template_name='pages/resultado_busqueda.html', context={'nss': res.nss, 'dni': res.dni, 'nombre': res.nombre, 'apellido1': res.apellido_1})
+            visitas = Visita.objects.filter(paciente__nss=res.nss)
+
+            context = {'nss': res.nss, 'dni': res.dni, 'nombre': res.nombre, 'apellido1': res.apellido_1, 'apellido2': res.apellido_2, 'direccion': res.direccion, 'cp': res.cp,
+                    'telefono': res.telefono, 'ciudad': res.ciudad, 'provincia': res.provincia, 'pais': res.pais, 'fecha_nacimiento': res.fecha_nacimiento, 'sexo': res.sexo,
+                    'visitas': visitas}
+
+            return render(request, 'pages/ficha_de_paciente.html', context)
     else:
         form = AltaForm()
 
     return render(request, 'pages/buscar_paciente.html', {'form': form})
-
-
-@login_required
-def get_visitas(request, nss):
-    """ All the Visitas objects matching
-    the given NSS are rendered """
-    if request.method == 'GET':
-        res = Visita.objects.filter(paciente__nss=nss)
-        return render(request, template_name='pages/resultado_visita.html', context={'resultados': res})
 
 
 @login_required
@@ -117,24 +116,3 @@ def cambiar_visita(request, id):
         form = VisitaForm(initial={'id': obj.id, 'fecha': obj.fecha, 'valorINR': obj.valorINR,
                                    'dosis': obj.dosis, 'duracion': obj.duracion, 'peso': obj.peso, 'rango': obj.rango, 'paciente': obj.paciente, 'comentario': obj.comentario, 'medicacion': obj.medicacion})
     return render(request, 'pages/modificar_visitas.html', {'form': form, 'id': id})
-
-
-@login_required
-def get_datos_demograficos(request, nss):
-    """ Connect to the remote database and
-    retrieve the data from the database to render
-    it on the client. No data is stored locally"""
-
-    if request.method == 'GET':
-        connection = MySQLDriver.MySQLConn(
-            host="localhost", database="usuariossanitarios", username="root", password="root", port=3306)
-        cursor = connection.cursor
-
-        query = 'SELECT nss, dni, nombre, apellido1, apellido2, direccion, cp, telefono, ciudad, provincia, pais, fecha_nacimiento, sexo FROM pacientes WHERE nss=%s' % nss
-        cursor.execute(query)
-        row = cursor.fetchone()
-        context = {'nss': row[0], 'dni': row[1], 'nombre': row[2], 'apellido1': row[3], 'apellido2': row[4], 'direccion': row[5], 'cp': row[
-            6], 'telefono': row[7], 'ciudad': row[8], 'provincia': row[9], 'pais': row[10], 'fecha_nacimiento': row[11], 'sexo': row[12]}
-        return render(request, 'pages/datos_demograficos.html', context)
-    else:
-        return HttpResponse('Method not allowed')
