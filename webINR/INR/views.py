a4f7d1a38a120493350100e257c82821664a9878
Antonio B <makishinake@hotmail.com>
2017-05-02 16:52:30 +0200

    Plantilla para error 404

--- webINR/INR/views.py
+++ webINR/INR/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# -*- encoding: utf-8 -*-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from .forms import AltaForm, VisitaForm
 from webINR import MySQLDriver
@@ -7,30 +8,35 @@ from models import PacienteClinica, Visita
 import random
 import string
 
+## @login_required es un decorador que indica que para acceder a la vista debemos estar logueados en el sistema
+## De no ser así, nos devolverá la pantalla de inicio de sesión.
 
 @login_required
 def index(request):
     return render(request, 'base.html')
 
-
 @login_required
 def ver_ficha(request):
+    ## Nos muestra la página ficha_de_paciente.html
     return render(request, 'pages/ficha_de_paciente.html')
 
 
 @login_required
 def dar_alta(request):
-    """ A connection tot he remote server is issued,
+    """ A connection to the remote server is issued,
     in order to check whether the data given exists. In case
     it doesn't exist a new user is created. """
-    if request.method == 'POST':
+
+    if request.method == 'POST':  ## Cuando enviemos datos a través del formulario...
         form = AltaForm(request.POST)
 
-        if form.is_valid():
+        if form.is_valid():  ## ...si el formulario es válido...
+            ## ...establecemos conexión con la base de datos.
             connection = MySQLDriver.MySQLConn(
                 host="localhost", database="usuariossanitarios", username="root", password="angel", port=3306)
             cursor = connection.cursor
 
+            ## La query cambiará según estemos buscando por dni o por nss
             if form.cleaned_data['query_choice'] == 'dni':
                 query = 'SELECT nss,dni,nombre,apellido1 FROM pacientes WHERE dni=\"%s\"' % form.cleaned_data[
                     'dni']
@@ -38,10 +44,17 @@ def dar_alta(request):
                 query = 'SELECT nss,dni,nombre,apellido1 FROM pacientes WHERE nss=\"%s\"' % form.cleaned_data[
                     'nss']
 
+            ## Ejecutamos la query
             cursor.execute(query)
+
+            ## Recuperamos el resultado de la query
             row = cursor.fetchone()
+
+            ## Creamos una contraseña aleatoria
             password = ''.join(random.choice(string.lowercase)
                                for i in range(10))
+
+            ## Creamos el usuario usando los parámetros obtenidos de la query o lo recuperamos en caso de ya existir (get_or_create())
             PacienteClinica.objects.get_or_create(
                 nss=row[0], dni=row[1], nombre=row[2], apellido_1=row[3], password=password)
 
@@ -50,6 +63,8 @@ def dar_alta(request):
     else:
         form = AltaForm()
 
+    ## Por defecto, la vista dar_alta nos muestra la página alta_de_paciente.html
+    ## Dentro de ésta página encontramos un formulario 'form' que se corresponde con la clase AltaForm dentro de forms.py
     return render(request, 'pages/alta_de_paciente.html', {'form': form})
 
 
@@ -61,12 +76,13 @@ def buscar(request):
         form = AltaForm(request.POST)
 
         if form.is_valid():
+            ## Sobre get_object_or_404(): https://docs.djangoproject.com/es/1.11/topics/http/shortcuts/#django.shortcuts.get_object_or_404
             if form.cleaned_data['query_choice'] == 'dni':
-                res = PacienteClinica.objects.get(dni=form.cleaned_data['dni'])
-                return render(request, template_name='pages/resultado_busqueda.html', context={'nss': res.nss, 'dni': res.dni, 'nombre': res.nombre, 'apellido1': res.apellido_1})
+                res = get_object_or_404(PacienteClinica, dni=form.cleaned_data['dni'])
             else:
-                res = PacienteClinica.objects.get(nss=form.cleaned_data['nss'])
-                return render(request, template_name='pages/resultado_busqueda.html', context={'nss': res.nss, 'dni': res.dni, 'nombre': res.nombre, 'apellido1': res.apellido_1})
+                res = get_object_or_404(PacienteClinica, nss=form.cleaned_data['nss'])
+
+            return render(request, template_name='pages/resultado_busqueda.html', context={'nss': res.nss, 'dni': res.dni, 'nombre': res.nombre, 'apellido1': res.apellido_1})
     else:
         form = AltaForm()
 
@@ -84,10 +100,10 @@ def get_visitas(request, nss):
 
 @login_required
 def cambiar_visita(request, id):
-    """ Allowing for Visita objects modification, 
-    including all the parameters belonging to the 
+    """ Allowing for Visita objects modification,
+    including all the parameters belonging to the
     given patient """
-    
+
     obj = Visita.objects.get(id=id)
     if request.method == 'POST':
         form = VisitaForm(request.POST, instance=obj)
@@ -105,10 +121,10 @@ def cambiar_visita(request, id):
 
 @login_required
 def get_datos_demograficos(request, nss):
-    """ Connect to the remote database and 
+    """ Connect to the remote database and
     retrieve the data from the database to render
     it on the client. No data is stored locally"""
-    
+
     if request.method == 'GET':
         connection = MySQLDriver.MySQLConn(
             host="localhost", database="usuariossanitarios", username="root", password="root", port=3306)
