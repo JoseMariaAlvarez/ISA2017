c3f1812d269ce5ad5fa35012a6c5e840f61bdbf1
Francisco <franferlop93@gmail.com>
2017-05-29 18:14:37 +0200

    Cambio importante de la primera iteración: se han cambiado las vista de buscar, ficha_paciente, se ha creado gestor_de_pacientes, se ha corregido la creación de las visitas, en gestor_de_paciente se puede visualizar el último paciente buscado.

--- webINR/INR/forms.py
+++ webINR/INR/forms.py
@@ -1,31 +1,41 @@
 # -*- encoding: utf-8 -*-
 from django import forms
-from .models import Visita
+from .models import Visita, Comentario
+
 
 # Alta Form.
 class AltaForm(forms.Form):
-    SEARCH_CHOICES = [('dni', 'DNI'),
-                      ('nss', 'NSS')]
-    query_choice = forms.ChoiceField(
-        label='Escoja tipo de busqueda', choices=SEARCH_CHOICES, widget=forms.RadioSelect())
-    nss = forms.CharField(label='NSS', max_length=20, widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
-    dni = forms.CharField(label='DNI', max_length=10, widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
+
+    dato = forms.CharField(label='DNI o NSS', max_length=20, widget=forms.TextInput(attrs={'class' : 'form-control'}), required=True)
 
 class VisitaForm(forms.ModelForm):
+    prefix = 'visita'
     class Meta:
         model = Visita
-        fields = '__all__'
+        fields = ['paciente', 'valorINR', 'dosis', 'duracion', 'peso', 'fecha','medicacion']
         labels = {
             'paciente': 'Paciente',
+            'duracion': 'Duración (días)',
+            'peso': 'Peso (kg)',
+            'dosis': 'Dosis (mg/semana)',
+            'valorINR' : 'Valor INR',
         }
         widgets = {
-            'paciente': forms.Select(attrs={'class' : 'form-control'}),
+            'paciente': forms.HiddenInput(attrs={'class' : 'form-control'}),
             'valorINR': forms.TextInput(attrs={'class' : 'form-control'}),
             'dosis': forms.TextInput(attrs={'class' : 'form-control'}),
-            'fecha': forms.TextInput(attrs={'class' : 'form-control'}),
-            'duracion': forms.TextInput(attrs={'class' : 'form-control'}),
+            'fecha': forms.TextInput(attrs={'class' : 'form-control', 'type':'date'}),
+            'duracion': forms.TextInput(attrs={'class' : 'form-control',}),
             'peso': forms.TextInput(attrs={'class' : 'form-control'}),
-            'rango': forms.TextInput(attrs={'class' : 'form-control'}),
-            'comentario': forms.Select(attrs={'class' : 'form-control'}),
             'medicacion': forms.Select(attrs={'class' : 'form-control'}),
         }
+
+class ComentarioVisitaForm(forms.ModelForm):
+    prefix = 'comentario'
+    class Meta:
+        model = Comentario
+        fields = '__all__'
+        labels = {'texto': 'Comentario'}
+        widgets = {
+            'texto' : forms.TextInput(attrs={'class' : 'form-control'}),
+            }
