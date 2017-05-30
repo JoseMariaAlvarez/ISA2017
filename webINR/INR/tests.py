dbf30281f7134df16486c22c028f045fbda6dbb0
Antonio Benitez <antbenhid@uma.es>
2017-05-30 23:42:33 +0200

    Añadidos algunos test unitarios para la base de datos

--- webINR/INR/tests.py
+++ webINR/INR/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.db import IntegrityError
+from .models import PacienteClinica, Medicacion
+from .forms import AltaForm
 
-# Create your tests here.
+class BDTest(TestCase):
+    def create_patient(self):
+        return PacienteClinica(
+            nss='1234', password='p1', dni='123A',nombre='person1',
+            apellido_1='s1',apellido_2='s2',direccion='c/etc',cp='2009',
+            telefono='65000001',ciudad='MAL',provincia='MAL',pais='MAL',
+            fecha_nacimiento='2001-01-01',rango='2.3',sexo=0)
+
+    def test_valid_form_alta(self):
+        p1 = self.create_patient()
+        p1.save()
+
+        data = {'dato': p1.nss}
+        form = AltaForm(data=data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form_alta_charfield_too_big(self):
+        data = {'dato': '123456789012345678901234567890'}
+        form = AltaForm(data=data)
+
+        self.assertFalse(form.is_valid())
+
+    def test_patient_isinstance(self):
+        p1 = self.create_patient()
+
+        self.assertTrue(isinstance(p1, PacienteClinica))
+
+    def test_unique_person_constrain(self):
+        p1 = self.create_patient()
+        p1.save()
+
+        p2 = self.create_patient()
+
+        self.assertRaises(IntegrityError, p2.save())
